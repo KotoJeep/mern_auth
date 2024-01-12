20f8b2d9425d74f6670b8a1b2cdb48298f19f17e
Anton Volkov <omaigader@gmail.com>
2024-01-12 15:20:43 +0700

    init project

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import userRoutes from "./routes/userRoutes.js";
+dotenv.config()
+
+const port = process.env.PORT || 8000
+
+const app = express()
+app.use('api/users', userRoutes)
+
+app.get('/', (req, res) => res.send('server is ready'))
+
+app.listen(port, () => console.log(`server started at ${port}`))
+
