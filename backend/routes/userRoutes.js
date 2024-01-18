bc8998fefb7f8a0e68353e38b8850f678d78363a
Anton Volkov <omaigader@gmail.com>
2024-01-18 16:30:36 +0700

    add database url

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,13 @@
 import express from "express";
-import {authUser} from "../controllers/userController.js";
+import {authUser, getUserProfile, logoutUser, registerUser, updateUserProfile} from "../controllers/userController.js";
+
 const router = express.Router()
 
-router.post('/auth', authUser)
+router.post('/', authUser);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.post('/register', registerUser)
+router.route('/profile').get(getUserProfile).put(updateUserProfile)
+
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from "./routes/userRoutes.js";
+import {errorHandler, notFound} from "./middleware/errorMiddleware.js";
 dotenv.config()
 
 const port = process.env.PORT || 8000
 
 const app = express()
-app.use('api/users', userRoutes)
+app.use('/api/users', userRoutes)
 
 app.get('/', (req, res) => res.send('server is ready'))
 
+app.use(notFound)
+app.use(errorHandler)
 app.listen(port, () => console.log(`server started at ${port}`))
 
